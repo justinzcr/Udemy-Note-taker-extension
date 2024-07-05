dc6778eb1197a7d238ed0a67ecf1f2eaffb60bd1
justinzcruiz <justinzcruiz@gmail.com>
2024-07-05 11:35:00 +0300

    Modified noteContent.js to debug

--- noteContent.js
+++ noteContent.js
@@ -1,15 +1,21 @@
+let spacePressCount = 0;
+
 document.addEventListener('keydown', function(event) {
-    if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT') {
-      if (event.code === 'Space') {
+  if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT') {
+    if (event.code === 'Space') {
+      spacePressCount++;
+      if (spacePressCount === 2) {
+        spacePressCount = 0;
         chrome.runtime.sendMessage({ action: "playVideo" });
-      } else if (event.key === 's' && event.key === 's' && event.key === 'd' && event.key === 't') {
-        chrome.runtime.sendMessage({ action: "captureScreenshot" });
       }
+    } else if (event.key === 's' && event.key === 's' && event.key === 'd' && event.key === 't') {
+      chrome.runtime.sendMessage({ action: "captureScreenshot" });
     }
-  });
-  
-  document.addEventListener('focusin', function(event) {
-    if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT') {
-      chrome.runtime.sendMessage({ action: "pauseVideo" });
-    }
-  });  
+  }
+});
+
+document.addEventListener('focusin', function(event) {
+  if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT') {
+    chrome.runtime.sendMessage({ action: "pauseVideo" });
+  }
+});
