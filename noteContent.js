40a7a06e75d2085ac0ca29b0d01999509b96fe26
justinzcruiz <justinzcruiz@gmail.com>
2024-07-14 21:48:04 +0300

    add console logs different from before to debug the problem

--- noteContent.js
+++ noteContent.js
@@ -1,36 +1,26 @@
-let isTyping = false;
 let spacePressCount = 0;
 
 document.addEventListener('keydown', function(event) {
-    console.log('Key pressed:', event.key);
-    if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT') {
+  console.log("Key down in note-taking tab:", event.key);
+  
+  if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT') {
     if (event.code === 'Space') {
       spacePressCount++;
       if (spacePressCount === 2) {
-        console.log('Spacebar pressed');
         spacePressCount = 0;
         chrome.runtime.sendMessage({ action: "playVideo" });
       }
     } else {
-      console.log('Typing detected');
       spacePressCount = 0;
-      isTyping = true;
       chrome.runtime.sendMessage({ action: "pauseVideo" });
     }
   }
 });
 
 document.addEventListener('focusin', function(event) {
-    console.log('Focus in:', event.target.tagName);
-    if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT') {
-    isTyping = true;
+  console.log("Focus in on element:", event.target.tagName);
+  
+  if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT') {
     chrome.runtime.sendMessage({ action: "pauseVideo" });
-  }
-});
-
-document.addEventListener('focusout', function(event) {
-    console.log('Focus out:', event.target.tagName);
-    if (event.target.tagName === 'TEXTAREA' || event.target.tagName === 'INPUT') {
-    isTyping = false;
   }
 });
--- udemyContent.js
+++ udemyContent.js
@@ -1,7 +1,9 @@
 document.addEventListener('keydown', function(event) {
-    if (event.ctrlKey && event.key === ',') {
-      chrome.runtime.sendMessage({ action: "rewindVideo" });
-    } else if (event.ctrlKey && event.key === '.') {
-      chrome.runtime.sendMessage({ action: "forwardVideo" });
-    }
-  });  
+  console.log("Key down in Udemy tab:", event.key);
+  
+  if (event.ctrlKey && event.key === ',') {
+    chrome.runtime.sendMessage({ action: "rewindVideo" });
+  } else if (event.ctrlKey && event.key === '.') {
+    chrome.runtime.sendMessage({ action: "forwardVideo" });
+  }
+});
